6d12c0dcf69be5e17c8f4299cc9934424092f702
Victor <47047497+173716414@users.noreply.github.com>
2023-11-16 10:44:39 +0800

    全局权限管理优化

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,6 @@ export default { components: { BasicLayout }, };
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
 import { onMounted } from "vue";
 
 // 全局初始化函数
@@ -22,18 +20,5 @@ const doInit = () => {
 
 onMounted(() => {
   doInit();
-});
-
-const router = useRouter();
-const store = useStore();
-
-router.beforeEach((to, from, next) => {
-  if (to.meta?.access === "canAdmin") {
-    if (store.state.user.loginUser?.role !== "admin") {
-      next("/noAuth");
-      return;
-    }
-  }
-  next();
 });
 </script>
