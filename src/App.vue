4bada4192febc3878ab93b6561b21a4fb48c5f70
Victor <47047497+173716414@users.noreply.github.com>
2023-11-16 10:58:51 +0800

    多套布局实现，用户登录页面实现

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 export default { components: { BasicLayout }, };
 <template>
   <div id="app">
-    <BasicLayout />
+    <template v-if="route.path.startsWith('/user')">
+      <router-view />
+    </template>
+    <template v-else>
+      <BasicLayout />
+    </template>
   </div>
 </template>
 
@@ -12,7 +17,9 @@ export default { components: { BasicLayout }, };
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
 import { onMounted } from "vue";
+import { useRoute } from "vue-router";
 
+const route = useRoute();
 // 全局初始化函数
 const doInit = () => {
   console.log("欢迎来到Voj");
